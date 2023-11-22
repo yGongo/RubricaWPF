8fb0a7e1f1c07d0b3b6ebc9aed800fad8a4769ee
Eugenio Malenchi <114256839+yGongo@users.noreply.github.com>
2023-11-22 10:09:50 +0100

    Add files via upload

---
+++ Eugenio.Malenchi.4i.WPF/Contatto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eugenio.Malenchi._4i.WPF
+{
+    internal class Contatto
+    {
+        private int _numero;
+        private string _cognome;
+
+        public int Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new ArgumentOutOfRangeException();
+
+                _numero = value;
+            }
+        }
+
+        public string Nome { get; set; }
+        public string EMail { get; set; }
+        public string Telefono { get; set; }
+        public string Citta { get; set; }
+        public string CAP { get; set; }
+
+        public string Cognome { get => _cognome; set => _cognome = value; }
+
+        public Contatto() { }
+
+        public Contatto(int numero, string nome, string cognome)
+        {
+            Numero = numero;
+            Nome = nome;
+            Cognome = cognome;
+        }
+
+        public Contatto (string riga)
+        {
+
+            string[] campi = riga.Split(';');
+            this.Nome = campi[0];
+            this.Cognome = campi[1];
+            this.Telefono = campi[2];
+            this.EMail = campi[3];
+            this.CAP = campi[4];
+
+
+        }
+    }
+}
---
+++ Eugenio.Malenchi.4i.WPF/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Eugenio.Malenchi._4i.WPF
+{ 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {   
+            InitializeComponent();
+            List<Contatto> listContatti = new List<Contatto>();
+            Contatto[] Contatti = new Contatto[100];
+            
+
+            try
+            {
+                for (int i = 0;i<Contatti.Length;i++)
+                {
+                    Contatti[i] = new Contatto();
+                }
+                StreamReader fin = new StreamReader("dati.csv");
+                while (!fin.EndOfStream)
+                {
+                     string? riga = fin.ReadLine();
+                    Contatto c = new Contatto(riga);
+                    listContatti.Add(c);
+
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Errore: " + ex.Message); 
+            }
+            
+            try
+            {
+                Contatto c = new Contatto
+                {
+                    Numero = 1,
+                    Nome = "Maurizio",
+                    Cognome = "Conti",
+                    EMail = "maurizio.conti@ittsrimini.edu.it",
+                    Telefono = "3337722",
+                    CAP = "47923"
+                };
+                listContatti.Add(c);
+
+                Contatto c1 = new Contatto(2, "Riccardo", "Bianchi");
+                listContatti.Add(c1);
+
+                Contatto c2 = new Contatto(3, "Antonio", "Vallone");
+                listContatti.Add(c2);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Errore: " + err.Message);
+            }
+
+
+            private void dgDati_LoadignRow(object sender, EventArgs e) {  }
+
+            
+        }
+    }
+}
